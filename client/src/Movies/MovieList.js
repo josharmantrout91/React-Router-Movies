3b5fe9db5a1547e9b520aaf381ea3500829430b3
josharmantrout91 <josh.armantrout@gmail.com>
2019-01-29 18:43:44 -0500

    Pt 1 Objectives
    - App wrapped in Router in index.js
    - Route for / that loads Movielist added
    - Route for /movies/:movieId that loads Movie added
    
    - Pt 2 Objectives
    
    - Each card in MovieList is a link that directs user to /movie/{id of specified movie}
    - Home button navigates back to / path from SavedList

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -11,12 +12,12 @@ export default class MovieList extends Component {
 
   componentDidMount() {
     axios
-      .get('http://localhost:5000/api/movies')
+      .get("http://localhost:5000/api/movies")
       .then(response => {
         this.setState(() => ({ movies: response.data }));
       })
       .catch(error => {
-        console.error('Server Error', error);
+        console.error("Server Error", error);
       });
   }
 
@@ -35,7 +36,9 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <Link to={`/movie/${movie.id}`}>
+        <h2>{title}</h2>
+      </Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
